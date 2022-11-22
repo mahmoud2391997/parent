0f9b7221931cd73d00c41d2842331154352b8447
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2022-11-22 23:37:28 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'splash_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DD School',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SpleashScreen(),
+    );
+  }
+}
