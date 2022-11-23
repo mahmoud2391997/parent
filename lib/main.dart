04c0a7fcd19521ec1e10b3764e97777ef254391d
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2022-11-23 14:00:01 +0200

    debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'splash_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,12 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   void initState() {
+    
     SystemChrome.setEnabledSystemUIOverlays([]);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DD School',
       theme: ThemeData(
         primarySwatch: Colors.blue,
